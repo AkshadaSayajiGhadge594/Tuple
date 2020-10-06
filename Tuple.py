7600b09efd302fbdef1589671cf91dc8131997c1
Akshada Sayaji Ghadge <61385821+AkshadaSayajiGhadge594@users.noreply.github.com>
2020-10-06 19:41:32 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,17 @@
+print("-------Demonstration of Tuple--------");
+
+tup=(11,"Akshada",308,225,"Ghadge")
+
+print(tup);
+print(tup[0]);
+print(tup[1]);
+print(tup[1:]);
+print(tup[:2]);
+print(tup[1:2]);
+
+#tup[1]="Akshada" It is not allowed to change the content
+
+print(len(tup));
+print("Akshada" in tup)
+
+del tup
